0f97590ab307dd042b961bee47a00e5810351a13
Emil Grużalski <emil.gruzalski@gmail.com>
2023-12-05 10:24:53 +0100

    Fix marking neighbors

--- knn.py
+++ knn.py
@@ -1,163 +1,76 @@
 import tkinter as tk
-import math
 from tkinter import filedialog
+import pandas as pd
+import numpy as np
+from scipy.spatial import distance
 
-class KNNClassifier:
-    def __init__(self, k, distance_metric, voting_type):
-        self.k = k
-        self.distance_metric = distance_metric
-        self.voting_type = voting_type
-        self.training_data = []
-
-    def load_training_data(self, filename):
-        with open(filename, 'r') as file:
-            for line in file:
-                x, y, category = map(float, line.strip().split(','))
-                self.training_data.append((x, y, category))
-
-    def normalize_data(self):
-        min_x = min(data[0] for data in self.training_data)
-        max_x = max(data[0] for data in self.training_data)
-        min_y = min(data[1] for data in self.training_data)
-        max_y = max(data[1] for data in self.training_data)
-
-        for i in range(len(self.training_data)):
-            x = (self.training_data[i][0] - min_x) / (max_x - min_x)
-            y = (self.training_data[i][1] - min_y) / (max_y - min_y)
-            self.training_data[i] = (x, y, self.training_data[i][2])
-
-    def classify_point(self, point):
-        distances = [(self.calculate_distance(point, data[:2]), data[2]) for data in self.training_data]
-        distances.sort(key=lambda x: x[0])
-        neighbors = distances[:self.k]
-
-        if self.voting_type == 'simple':
-            votes = [neighbor[1] for neighbor in neighbors]
-            majority_vote = max(set(votes), key=votes.count)
-            return majority_vote
-        elif self.voting_type == 'weighted':
-            total_weighted_votes = sum(1 / (distance[0] ** 2) * distance[1] for distance in neighbors)
-            return round(total_weighted_votes)
-
-    def calculate_distance(self, point1, point2):
-        if self.distance_metric == 'euclidean':
-            return math.sqrt((point1[0] - point2[0]) ** 2 + (point1[1] - point2[1]) ** 2)
-        elif self.distance_metric == 'manhattan':
-            return abs(point1[0] - point2[0]) + abs(point1[1] - point2[1])
-
-    def calculate_neighbors(self, point):
-        distances = [(self.calculate_distance(point, data[:2]), data[1]) for data in self.training_data]
-        distances.sort(key=lambda x: x[0])
-        neighbors = distances[:self.k]
-        colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']
-        result = [(data[0], data[1], colors[int(data[1])], data[0]) for data in neighbors]
-        return result
-
-class KNNApp:
+class KNN:
     def __init__(self, master):
         self.master = master
-        self.master.title("KNN Classifier")
-        self.canvas_width = 500
-        self.canvas_height = 500
-        self.canvas = tk.Canvas(self.master, width=self.canvas_width, height=self.canvas_height, bg='white')
+        self.canvas = tk.Canvas(self.master, width=600, height=600)
         self.canvas.pack()
-        self.knn_classifier = None
-        self.clicked_points = []
-        self.last_clicked_point = None
-        self.line_tags = []
+        self.data = None
+        self.norm_data = None
+        self.k = tk.IntVar(value=1)
+        self.metric = tk.StringVar(value='euclidean')
+        self.vote = tk.StringVar(value='simple')
+        self.colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange']
+        self.points = []
+        self.last_point = None
+        self.neighbors = []
 
-        self.create_widgets()
-
-    def create_widgets(self):
-        self.load_button = tk.Button(self.master, text="Load Training Data", command=self.load_training_data)
+        self.k_slider = tk.Scale(self.master, from_=1, to=20, orient='horizontal', variable=self.k)
+        self.k_slider.pack()
+        self.metric_menu = tk.OptionMenu(self.master, self.metric, 'euclidean', 'manhattan')
+        self.metric_menu.pack()
+        self.vote_menu = tk.OptionMenu(self.master, self.vote, 'simple', 'weighted')
+        self.vote_menu.pack()
+        self.load_button = tk.Button(self.master, text='Load data', command=self.load_data)
         self.load_button.pack()
 
-        self.k_label = tk.Label(self.master, text="Select k (1-20):")
-        self.k_label.pack()
-        self.k_entry = tk.Entry(self.master)
-        self.k_entry.pack()
+    def load_data(self):
+        filename = filedialog.askopenfilename()
+        self.data = pd.read_csv(filename, header=None)
+        self.norm_data = (self.data - self.data.min()) / (self.data.max() - self.data.min())
+        self.draw_points()
 
-        self.metric_label = tk.Label(self.master, text="Select Distance Metric:")
-        self.metric_label.pack()
-        self.metric_var = tk.StringVar()
-        self.metric_var.set('euclidean')
-        self.metric_dropdown = tk.OptionMenu(self.master, self.metric_var, 'euclidean', 'manhattan')
-        self.metric_dropdown.pack()
+    def draw_points(self):
+        self.canvas.delete('all')
+        for i in range(len(self.norm_data)):
+            x = self.norm_data.iloc[i, 0] * 550 + 25
+            y = self.norm_data.iloc[i, 1] * 550 + 25
+            color = self.colors[self.data.iloc[i, 2]]
+            self.canvas.create_oval(x-5, y-5, x+5, y+5, fill=color)
+        self.canvas.bind('<Button-1>', self.classify_point)
 
-        self.voting_label = tk.Label(self.master, text="Select Voting Type:")
-        self.voting_label.pack()
-        self.voting_var = tk.StringVar()
-        self.voting_var.set('simple')
-        self.voting_dropdown = tk.OptionMenu(self.master, self.voting_var, 'simple', 'weighted')
-        self.voting_dropdown.pack()
+    def classify_point(self, event):
+        x = (event.x - 25) / 550
+        y = (event.y - 25) / 550
+        point = pd.Series([x, y])
+        if self.metric.get() == 'euclidean':
+            dists = self.norm_data.iloc[:, :2].apply(lambda row: distance.euclidean(row, point), axis=1)
+        else:
+            dists = self.norm_data.iloc[:, :2].apply(lambda row: distance.cityblock(row, point), axis=1)
+        nearest = dists.nsmallest(self.k.get())
+        if self.vote.get() == 'simple':
+            votes = self.data.loc[nearest.index, 2].value_counts()
+        else:
+            weights = 1 / nearest**2
+            votes = self.data.loc[nearest.index, 2].groupby(weights.index).apply(lambda x: (x * weights).sum()).value_counts()
+        category = int(votes.idxmax())
+        color = self.colors[category]
+        if self.last_point is not None:
+            self.canvas.delete(self.last_point)
+        self.last_point = self.canvas.create_rectangle(event.x-5, event.y-5, event.x+5, event.y+5, outline=color)
+        for neighbor in self.neighbors:
+            self.canvas.delete(neighbor)
+        self.neighbors = []
+        for i in nearest.index:
+            nx = self.norm_data.iloc[i, 0] * 550 + 25
+            ny = self.norm_data.iloc[i, 1] * 550 + 25
+            self.neighbors.append(self.canvas.create_oval(nx-7, ny-7, nx+7, ny+7, outline='black'))
+            self.neighbors.append(self.canvas.create_text(nx, ny-10, text=f'{nearest[i]:.2f}', fill='black'))
 
-        self.classify_button = tk.Button(self.master, text="Classify", command=self.classify_point)
-        self.classify_button.pack()
-
-        self.canvas.bind("<Button-1>", self.click_event)
-
-    def load_training_data(self):
-        filename = filedialog.askopenfilename(title="Select Training Data File", filetypes=[("Text Files", "*.txt")])
-        if filename:
-            self.knn_classifier = KNNClassifier(1, 'euclidean', 'simple')
-            self.knn_classifier.load_training_data(filename)
-            self.knn_classifier.normalize_data()
-            self.display_training_data()
-
-    def display_training_data(self):
-        self.canvas.delete("all")
-        categories = set(data[2] for data in self.knn_classifier.training_data)
-        colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange']
-        for data in self.knn_classifier.training_data:
-            x, y, category = data
-            color = colors[int(category)]
-            self.canvas.create_oval(x * self.canvas_width - 5, y * self.canvas_height - 5,
-                                    x * self.canvas_width + 5, y * self.canvas_height + 5, fill=color)
-
-    def click_event(self, event):
-        x = event.x / self.canvas_width
-        y = event.y / self.canvas_height
-        category = self.knn_classifier.classify_point((x, y))
-        neighbors = self.knn_classifier.calculate_neighbors((x, y))
-
-        if self.last_clicked_point:
-            # Usuń poprzednie linie
-            self.canvas.delete(*self.line_tags)
-            self.line_tags = []
-
-        self.last_clicked_point = (x, y, category, neighbors)
-        self.clicked_points.append(self.last_clicked_point)
-        self.display_clicked_points()
-
-    def display_clicked_points(self):
-        self.canvas.delete("lines")
-        for point in self.clicked_points:
-            x, y, category, neighbors = point
-            color = ['red', 'blue', 'green', 'yellow', 'purple', 'orange'][int(category)]
-            self.canvas.create_rectangle(x * self.canvas_width - 5, y * self.canvas_height - 5,
-                                         x * self.canvas_width + 5, y * self.canvas_height + 5, fill=color)
-                    
-        if self.last_clicked_point:
-            x, y, _, _ = self.last_clicked_point
-            for neighbor in neighbors:
-                nx, ny, ncolor, distance = neighbor
-                self.canvas.create_line(x * self.canvas_width, y * self.canvas_height,
-                                        nx * self.canvas_width, ny * self.canvas_height, fill=ncolor, tags="lines")
-                self.canvas.create_text((x * self.canvas_width + nx * self.canvas_width) / 2,
-                                        (y * self.canvas_height + ny * self.canvas_height) / 2,
-                                        text=f'{distance:.2f}', fill='black', tags="lines")
-
-    def classify_point(self):
-        if self.knn_classifier:
-            k = int(self.k_entry.get())
-            distance_metric = self.metric_var.get()
-            voting_type = self.voting_var.get()
-            self.knn_classifier.k = k
-            self.knn_classifier.distance_metric = distance_metric
-            self.knn_classifier.voting_type = voting_type
-            self.display_clicked_points()
-
-if __name__ == "__main__":
-    root = tk.Tk()
-    app = KNNApp(root)
-    root.mainloop()
+root = tk.Tk()
+knn = KNN(root)
+root.mainloop()
